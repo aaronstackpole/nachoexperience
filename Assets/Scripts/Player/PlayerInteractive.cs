75c6fc0de5d5fee97cf1e1e4b14edf6b1270dfda
Aaron Stackpole <aaron@carryingthewhale.com>
2019-01-27 15:08:43 -0600

    Interactables updates

--- Assets/Scripts/Player/PlayerInteractive.cs
+++ Assets/Scripts/Player/PlayerInteractive.cs
@@ -2,15 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerInteractive : MonoBehaviour {
+public class PlayerInteractive : MonoBehaviour
+{
+    public List<GameObject> Interactables;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public string GetInteractableQuip(GameObject worldObject, string clickedApp)
+    {
+        foreach (var thing in Interactables)
+        {
+            if (thing == worldObject)
+            {
+                switch (clickedApp)
+                {
+                    case "":
+                    default:
+                        return null;
+                }
+            }
+        }
+        return null;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        Interactables.Add(GameObject.Find("TacoVendorClick"));
+        Interactables.Add(GameObject.Find("ResidentialAlleyClick"));
+
+        Interactables.Add(GameObject.Find("LatinMusicDoor"));
+        Interactables.Add(GameObject.Find("HouseOfBluesDoor"));
+        Interactables.Add(GameObject.Find("TraderJanesDoor"));
+        Interactables.Add(GameObject.Find("ChowsChowDoor"));
+        Interactables.Add(GameObject.Find("CommercialAlleyClick"));
+
+        Interactables.Add(GameObject.Find("StadiumClick"));
+        Interactables.Add(GameObject.Find("WheelClick"));
+        Interactables.Add(GameObject.Find("BeachClick"));
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
 		
 	}
 }
